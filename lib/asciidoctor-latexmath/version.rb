b237a1e02072b3b1ca42488f88f83685ec5325b8
Shuai Zhang <zhangshuai.ustc@gmail.com>
2025-10-07 05:32:45 +0000

    fix: Use Correct Semantic Version

--- lib/asciidoctor-latexmath/version.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-# SPDX-FileCopyrightText: 2025 Shuai Zhang
-#
-# SPDX-License-Identifier: LGPL-3.0-or-later WITH LGPL-3.0-linking-exception
-
-require_relative "../asciidoctor/latexmath/version"
--- lib/asciidoctor/latexmath/version.rb
+++ lib/asciidoctor/latexmath/version.rb
@@ -6,6 +6,6 @@
 
 module Asciidoctor
   module Latexmath
-    VERSION = "0.1.0"
+    VERSION = "2.0.0-alpha.1"
   end
 end
